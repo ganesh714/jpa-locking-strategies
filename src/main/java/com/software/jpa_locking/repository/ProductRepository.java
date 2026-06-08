d35047f5e89ebb038f079085da39e2269d23e1e9
ganesh714 <evvganesh1@gmail.com>
2026-06-08 10:30:39 +0530

    Update ProductRepository

--- src/main/java/com/software/jpa_locking/repository/ProductRepository.java
+++ src/main/java/com/software/jpa_locking/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.software.jpa_locking.repository;
 
-import com.software.jpa_locking.entity.Product;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import com.software.jpa_locking.model.Product;
 
 import java.util.Optional;
 
