c414fe3548ed8cfea324ce62990eacfab93f6efc
ganesh714 <evvganesh1@gmail.com>
2026-06-08 10:15:48 +0530

    Add OrderRequest DTO

---
+++ src/main/java/com/software/jpa_locking/dto/OrderRequest.java
@@ -0,0 +1,14 @@
+package com.software.jpa_locking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private Long productId;
+    private int quantity;
+    private Long userId;
+}
