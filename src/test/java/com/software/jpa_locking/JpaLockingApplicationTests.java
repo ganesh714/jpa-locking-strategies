e0f70eba69090a8280638b47f0dae33bd9565da9
ganesh714 <evvganesh1@gmail.com>
2026-06-08 09:59:04 +0530

    Initial commit

---
+++ src/test/java/com/software/jpa_locking/JpaLockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.jpa_locking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
