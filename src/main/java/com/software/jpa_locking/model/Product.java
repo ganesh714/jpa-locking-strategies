7bb31710f7ddde870e992b34c6c06a5c6ab368b7
ganesh714 <evvganesh1@gmail.com>
2026-06-08 10:15:48 +0530

    Add Product model

---
+++ src/main/java/com/software/jpa_locking/model/Product.java
@@ -0,0 +1,31 @@
+package com.software.jpa_locking.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int stock;
+
+    @Version
+    private Long version;
+}
