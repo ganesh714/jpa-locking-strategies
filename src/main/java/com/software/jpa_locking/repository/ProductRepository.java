3cc08510314de229fc579857fa4b6e06397ec96c
ganesh714 <evvganesh1@gmail.com>
2026-06-13 08:58:44 +0530

    Update ProductRepository

--- src/main/java/com/software/jpa_locking/repository/ProductRepository.java
+++ src/main/java/com/software/jpa_locking/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.software.jpa_locking.repository;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,4 +16,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM Product p WHERE p.id = :id")
     Optional<Product> findByIdWithPessimisticLock(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE Product p SET p.stock = :stock, p.version = 0 WHERE p.id = :id")
+    void resetProductStockAndVersion(@Param("id") Long id, @Param("stock") int stock);
 }
